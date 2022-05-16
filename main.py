112d91e20fa5d6c092c695cbe7527e0f35fefd43
hyu-nani <cube_h2@naver.com>
2022-05-16 23:04:23 +0900

    작업본 옮김

---
+++ main.py
@@ -0,0 +1,15 @@
+#pip install flask
+#pip install flask-login
+#pip install flask-sqlalchemy
+#pip install waitress
+developMode = True
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    if developMode:
+        app.run(debug=True, host="0.0.0.0", port=8080)
+    else:
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=8080)
---
+++ website/auth.py
@@ -0,0 +1,27 @@
+
+import re
+from flask import Blueprint, render_template, request
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.form
+    print(data)
+    return render_template("login.html", boolean=True)
+
+
+@auth.route('/logout')
+def logout():
+    return render_template("logout.html")
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        address = request.form.get('address')
+    return render_template("sign_up.html")
