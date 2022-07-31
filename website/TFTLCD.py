df7f5b548357b0321116398f24a04ad7f5d5c1a7
HyeonHan Bae <baehyunhan3@gmail.com>
2022-07-31 17:37:34 +0900

    Create TFTLCD.py

---
+++ website/TFTLCD.py
@@ -0,0 +1,20 @@
+
+import RPi.GPIO as GPIO
+import time
+import os
+
+'''
+하드웨어 핀 연결도
+'''
+dataPin = [13,6,5,11,9,10,22,27,17] # D0~D7
+RS = 21
+CS = 16
+RD = 20
+WR = 26
+RES = 19
+BLK = 12
+
+GPIO.setMode(GPIO.BOARD)
+for i range(8):
+    GPIO.setup(dataPin[i], GPIO.OUT)
+    
